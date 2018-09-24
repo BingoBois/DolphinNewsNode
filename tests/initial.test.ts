45f5aba51a4acceff60eb996a34c5411018daf51
ExoMemphiz <ExoMemphiz@gmail.com>
2018-09-24 09:23:13 +0200

    Updated Tests

--- tests/initial.test.ts
+++ tests/initial.test.ts
@@ -1,3 +1,15 @@
 test('1 should equal 1', () => {
     expect(1).toBe(1);
 })
+
+const exampleObject = {
+    key: "Hello",
+    value: "Test"
+}
+
+test('Objects to be equal', () => {
+    expect(exampleObject).toMatchObject({
+        key: "Hello", 
+        value: "Test"
+    });
+})
