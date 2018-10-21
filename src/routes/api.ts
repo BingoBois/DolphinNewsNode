1727563473232e5d42ea2cd38b8abc815b40be34
William Pfaffe <william.pfaffe@gmail.com>
2018-10-21 22:01:47 +0200

    Changed api text

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,7 +7,7 @@ const router: Router = Router();
 //Standard Test Route
 router.get('/', (req: Request, res: Response) => {
   res.json({
-    message: 'Hello and welcome to my guide',
+    message: 'Hello and sut to my guide',
     error: undefined
 })
 });
