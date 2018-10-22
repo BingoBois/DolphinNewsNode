f63a0b2afb0916198b504e0c4b6f5a573c756362
Viktor Kim Christiansen <viktorkim2005@hotmail.com>
2018-10-22 11:14:16 +0200

    Update api.ts

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,7 +7,7 @@ const router: Router = Router();
 //Standard Test Route
 router.get('/', (req: Request, res: Response) => {
   res.json({
-    message: 'Hello and øøøhhmmmm sut my øhhhhmmm to my guide',
+    message: 'Dolphin News Backend',
     error: undefined
 })
 });
