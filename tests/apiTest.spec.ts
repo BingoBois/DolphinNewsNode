ada79da6f5613e58e9be16ef63f1bfdb75399776
Zaeem96 <zaeem1996@hotmail.com>
2018-10-22 10:11:08 +0200

    corrected mistakes

--- tests/apiTest.spec.ts
+++ tests/apiTest.spec.ts
@@ -1,10 +1,12 @@
 const request = require("supertest");
 
-describe('GET / - a simple api endpoint', () => {
-  it('Hello API Request', async () => {
-    const result = await request("http://80.240.24.203:3000/post/get/All").get('/');
-    console.log()
-    //expect(result.text).toEqual("hello");
+const URL = "http://80.240.24.203:3000"
+
+describe('GET / - an API-endpoint', () => {
+  it('should return all posts as JSON', async () => {
+    const result = await request(URL).get('/post/get/All');
+    console.log(typeof result.body.Post)
     expect(result.statusCode).toEqual(200);
+    expect(typeof result.body.Post).toEqual("object");
   });
 })
