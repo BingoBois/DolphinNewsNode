5570ee7a544c84fb9a842a2c817b9593e5e16ec3
Zaeem96 <zaeem1996@hotmail.com>
2018-10-22 10:15:19 +0200

    added method that tets that server is running

--- tests/apiTest.spec.ts
+++ tests/apiTest.spec.ts
@@ -2,6 +2,21 @@ const request = require("supertest");
 
 const URL = "http://80.240.24.203:3000"
 
+describe('GET / - check that server is running', () => {
+  it('should return statuscode 200', async () => {
+    const result = await request(URL).get('/');
+    expect(result.statusCode).toEqual(200);
+  });
+})
+
+describe('GET / - an API-endpoint', () => {
+  it('should return all posts as JSON', async () => {
+    const result = await request(URL).get('/post/get/All');
+    expect(result.statusCode).toEqual(200);
+    expect(typeof result.body.Post).toEqual("object");
+  });
+})
+
 describe('GET / - an API-endpoint', () => {
   it('should return all posts as JSON', async () => {
     const result = await request(URL).get('/post/get/All');
