cbcd5bc08eaf1725399a2d11fb0b9d9afdba2ec8
Viterkim <viktorkim2005@hotmail.com>
2018-10-27 18:18:05 +0200

    fixed long timeout badly...

--- src/controllers/mysql/connection.ts
+++ src/controllers/mysql/connection.ts
@@ -7,12 +7,18 @@ let connection: Connection = mysql.createConnection({
     database: 'dolphinnews'
 });
 
+
 connection.connect((err: Error) => {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-    console.log(`connected as id ${connection.threadId}`);
-  });
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+connection.on('error', function() {
+  console.log("Reconnecting");
+  connection.connect();
+});
 
 export default connection;
