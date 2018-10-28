1dd5669afda3cc3ef519ed6a6dc6751d3da15b74
RadeonXRay <ceo_titanic@msn.com>
2018-10-28 02:24:49 +0200

    Minor updates

--- src/controllers/mysql/queries/commentsQueries.ts
+++ src/controllers/mysql/queries/commentsQueries.ts
@@ -6,7 +6,7 @@ import { resolve } from 'url';
 export function selectAllComments(){
     return new Promise((resolve) => {
     connection.query(
-    'SELECT   comment.`id` as commentId, comment.`content` AS commentContent, comment.`time` as commentTime, user.id as userId, user.username AS userName, comment.`fk_comment` as commentNumber, comment.`fk_post` as postID, post.title as postTitle, post.`url` as postURL, post.`time` as postTime' 
+    'SELECT   comment.`id` as commentId, comment.`content` AS commentContent, comment.`time` as commentTime, user.id as userId, user.username AS userName, comment.`fk_comment` as commentNumber, comment.`fk_post` as postId, post.title as postTitle, post.`url` as postURL, post.`time` as postTime' 
     +' FROM user ' 
     +' JOIN comment ON user.id = comment.`fk_user`' 
     +' join post ON post.`id` = comment.`fk_post`'
@@ -21,7 +21,7 @@ export function selectAllComments(){
   //Retrieves all the comments with (and only with) votes
   export function selectGetAllCommentsWithVotes() {
     return new Promise((resolve) => {
-        connection.query('Select vote_comment.`fk_comment` as commentID, comment.`content` as commentContent, user.`username` as userName, SUM(vote_comment.amount) as votesCount, post.id as postId, post.url as postURL from vote_comment join comment on comment.`id` = vote_comment.fk_comment join user on comment.`fk_user` = user.`id` join post on comment.`fk_post` = post.id GROUP BY vote_comment.fk_comment', (error, results, fields) => {
+        connection.query('Select vote_comment.`fk_comment` as commentId, comment.`content` as commentContent, user.`username` as userName, SUM(vote_comment.amount) as votesCount, post.id as postId, post.url as postURL from vote_comment join comment on comment.`id` = vote_comment.fk_comment join user on comment.`fk_user` = user.`id` join post on comment.`fk_post` = post.id GROUP BY vote_comment.fk_comment', (error, results, fields) => {
         let allComments = results;
         resolve(allComments);
       })
