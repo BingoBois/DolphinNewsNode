c349a816d1507077a46cda2a558797e3514a5102
ExoMemphiz <ExoMemphiz@gmail.com>
2018-11-08 17:57:42 +0100

    Updated latestDigested

--- src/routes/latestApi.ts
+++ src/routes/latestApi.ts
@@ -6,13 +6,7 @@ const router: Router = Router();
 //Get latestDigested
 router.get("/", (req, res) => {
   latestDigestedPostNumber().then((result: any) =>{
-      if(result.lastCommentId > result.lastPostId){
-        res.statusCode = 200
-        res.json(`${result.lastCommentId}`);
-      }else if(result.lastPostId > result.lastCommentId){
-        res.statusCode = 200
-        res.send(`${result.lastPostId}`);
-      }
+      res.send(`${Math.max(result.lastCommentId, result.lastPostId)}`);
   }).catch(e => res.send(e))
 })
 
