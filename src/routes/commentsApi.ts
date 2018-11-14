17a5ff13cabef76d241069fbe108fe6a3181bf2b
Zaeem96 <zaeem1996@hotmail.com>
2018-11-14 03:17:20 +0100

    fixed responses when voting

--- src/routes/commentsApi.ts
+++ src/routes/commentsApi.ts
@@ -3,15 +3,15 @@ import { selectAllComments, selectGetAllCommentsWithVotes, selectAllCommentsFrom
 import { vote, unVote } from '../controllers/mysql/queries/queries';
 import VoteObject from '../types/vote';
 const router: Router = Router();
- 
+
 //Get all comments with
-router.get('/get/all/withvote', (req: Request, res: Response) =>{
+router.get('/get/all/withvote', (req: Request, res: Response) => {
   selectGetAllCommentsWithVotes().then(resu => {
     res.json(JSON.stringify(resu));
   })
 });
- 
-router.get('/get/all', (req: Request, res: Response) =>{
+
+router.get('/get/all', (req: Request, res: Response) => {
   selectAllComments().then(resu => {
     res.json(JSON.stringify(resu));
   })
@@ -22,19 +22,33 @@ router.get('/get/bypost/:id', (req: Request, res: Response) => {
     res.json(JSON.stringify(resu));
   })
 });
- 
+
 router.post('/vote', (req: Request, res: Response) => {
   const tempVote: VoteObject = req.body;
   tempVote.vote_type = 'comment';
   tempVote.amount = 1;
-  vote(tempVote);
+  vote(tempVote)
+    .then(() => res.json({ succes: true }))
+    .catch((e) => {
+      res.statusCode = 500;
+      res.json({
+        error: 500
+      })
+    })
 });
 
 router.delete('/unvote/userId/:userId/commentId/:commentId', (req: Request, res: Response) => {
   const userId = req.params.userId;
   const commentId = req.params.commentId;
   const vote_type = 'comment';
-  unVote(userId, commentId, vote_type);
+  unVote(userId, commentId, vote_type)
+    .then(() => res.json({ succes: true }))
+    .catch((e) => {
+      res.statusCode = 500;
+      res.json({
+        error: 500
+      })
+    })
 });
- 
+
 export const commentsApi: Router = router;
